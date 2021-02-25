5e6891cd4e2117a826ccdcefd225ce8f4fddd169
Fayza Khan <70770111+fayza-khan@users.noreply.github.com>
2021-02-26 00:19:08 +0530

    Update Check-if-a-given-graph-is-tree-or-not.py

--- Check-if-a-given-graph-is-tree-or-not.py
+++ Check-if-a-given-graph-is-tree-or-not.py
@@ -12,10 +12,22 @@ An undirected graph is tree if it has following properties.
 For an undirected graph we can either use BFS or DFS to detect above two properties.
 
 -> How to detect cycle in an undirected graph? 
+
+To prove an undirected graph having no cycle, we can also do a DFS. If a graph contains a cycle, then we would visit a 
+certain node more than once. There is a minor caveat, since the graph is undirected, when we visit a child we would always 
+add parent to the next visit list. This creates a trivial cycle and not the real cycle we want. We can avoid detecting 
+trivial cycle but adding an additional parent state in the DFS call.
+
 For every visited vertex ‘v’, if there is an adjacent ‘u’ such that u is already 
 visited and u is not parent of v, then there is a cycle in graph. If we don’t find such an adjacent for any vertex, we say that 
 there is no cycle (See Detect cycle in an undirected graph for more details).
+
 -> How to check for connectivity? 
+
+Being connected means you can start from any node and reach any other node. To prove it, we can do a DFS and add each node we 
+visit to a set. After we visited all the nodes, we compare the number of nodes in the set with the total number of nodes. If 
+they are the same then every node is accessible from any other node and the graph is connected.
+
 Since the graph is undirected, we can start BFS or DFS from any vertex and check if all vertices are reachable or not. 
 If all vertices are reachable, then graph is connected, otherwise not.
  
